7823e6a8b13d97687d0e90816f5aa1fdbdcedde9
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-04 00:43:19 +0900

    feat: entity, moduel 추가
    - featureEntity 추가
      - reports, user
    - rootEntity 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm"
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { ReportsModule } from "./reports/reports.module";
+import { User } from "./users/user.entity";
+import { Reports } from "./reports/reports.entity";
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [User, Reports],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
