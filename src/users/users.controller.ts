e71501039447226f68b583ceb52f2d239751a0d8
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-06 01:03:57 +0900

    feat: controller, service추가
    - service crud 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dtos/create-user-dto";
+import { UsersService } from "./users.service";
 
-@Controller("users")
-export class UsersController {}
+@Controller("auth")
+export class UsersController {
+  constructor(private userServices: UsersService) {}
+  @Post("/signup")
+  createUser(@Body() body: CreateUserDto) {
+    this.userServices.create(body.email, body.password);
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,6 +7,6 @@ import { User } from "./user.entity";
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
