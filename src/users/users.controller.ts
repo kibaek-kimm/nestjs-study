839d004ec959c148de0becde9c29dc4038a63788
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-06 01:25:52 +0900

    feat: endpoint 추가
    - findAll, remove, update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { 
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from "@nestjs/common";
+import { UpdateUserDto } from "./dtos/update-user.dto";
 import { CreateUserDto } from "./dtos/create-user-dto";
 import { UsersService } from "./users.service";
 
@@ -8,5 +18,25 @@ export class UsersController {
   @Post("/signup")
   createUser(@Body() body: CreateUserDto) {
     this.userServices.create(body.email, body.password);
+  }
+
+  @Get("/:id")
+  findUser(@Param("id") id: string) {
+    return this.userServices.findOne(+id);
+  }
+
+  @Get("/")
+  findAllUsers(@Query("email") email: string) {
+    return this.userServices.find(email);
+  }
+
+  @Delete("/:id")
+  removeUser(@Param("id") id: string) {
+    return this.userServices.remove(+id);
+  }
+
+  @Patch("/:id")
+  updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+    return this.userServices.update(+id, body);
   }
 }
