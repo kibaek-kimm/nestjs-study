3a99d08488aaf26e61040156143f8f3423c469ea
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-07 21:44:23 +0900

    feat: AuthService 추가
    - mro를 위해 별도의 auth 서비스로 분리

---
+++ src/users/auth.service.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { UsersService } from "./users.service";
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const user = await this.usersService.find(email);
+    if (user.length) {
+      throw new BadRequestException("email in use");
+    }
+
+    // Hash the users password
+
+    // Create a new user and save it
+
+    // return the user
+  }
+
+  signin() {
+    // code
+  }
+}
