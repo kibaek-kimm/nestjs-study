beb6a09adb7618bc2cd75001ccfb5cd31ade7d8b
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-08 12:54:52 +0900

    feat: password 필드 hash+salt 조합으로 암호화 하여 저장하도록 수정

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,21 +1,35 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
   async signup(email: string, password: string) {
     // See if email is in use
-    const user = await this.usersService.find(email);
-    if (user.length) {
+    const existUser = await this.usersService.find(email);
+    if (existUser.length) {
       throw new BadRequestException("email in use");
     }
 
     // Hash the users password
+    // Generate a salt
+    const salt = randomBytes(8).toString("hex");
+
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed result and the salt together
+    const result = salt + "." + hash.toString("hex");
 
     // Create a new user and save it
+    const user = await this.usersService.create(email, result);
 
     // return the user
+    return user;
   }
 
   signin() {
