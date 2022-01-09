dbd8a0c5e7070ac48deff26e9ac3a5446f4a4796
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-09 17:56:03 +0900

    feat: signup, signin 세션 연동, signout, whoami 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Query,
   Delete,
   NotFoundException,
+  Session,
 } from "@nestjs/common";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { CreateUserDto } from "./dtos/create-user-dto";
@@ -23,14 +24,29 @@ export class UsersController {
     private userServices: UsersService,
     private authService: AuthService,
   ) {}
+
+  @Get("/whoami")
+  whoAmI(@Session() session: any) {
+    return this.userServices.findOne(session.userId);
+  }
+
+  @Post("/signout")
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Post("/signup")
-  createUser(@Body() body: CreateUserDto) {
-    this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post("/signin")
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get("/:id")
