7ceaaed6b8f9206efb2df9af2029c9a428787e77
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-10 23:07:03 +0900

    feat: auth.service test 추가
    - 기본 테스트환경 설정

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
+import { UsersService } from "./users.service";
+
+describe("AuthService", () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+    service = module.get(AuthService);
+  });
+
+  it("can create an instance of auth service", async () => {
+    // Create a face copy of the users service
+    expect(service).toBeDefined();
+  });
+});
