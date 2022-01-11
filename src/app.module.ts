dad2e4d04a91ec85115815076ac1acdc1a8d58c6
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-12 02:21:17 +0900

    fix: main.ts에 있는 설정 app.module로 이동
    - e2e테스트 설정에서 main.ts를 거치지 않고 app.module을 직접 참조하기 때문에 test를 위한 db설정을 할 수 없는 이슈 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,62 @@
-import { Module } from "@nestjs/common";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { MiddlewareConsumer, Module, ValidationPipe } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { ReportsModule } from "./reports/reports.module";
 import { User } from "./users/user.entity";
 import { Reports } from "./reports/reports.entity";
+const cookieSession = require("cookie-session");
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: "db.sqlite",
-      entities: [User, Reports],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        console.log(config);
+        return {
+          type: "sqlite",
+          database: config.get<string>("DB_NAME"),
+          entities: [User, Reports],
+          synchronize: true,
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: "sqlite",
+    //   database: process.env.DB_NAME,
+    //   entities: [User, Reports],
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ["secret-key"],
+        }),
+      )
+      .forRoutes("*");
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { NestFactory } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
-const cookieSession = require("cookie-session");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ["secret-key"],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(3000);
 }
 bootstrap();
