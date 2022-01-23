392b5d5eaf22cde243090e752d8fcac1adfcecba
Kibaek Kim <kibaek.kimm@gmail.com>
2022-01-23 18:16:04 +0900

    feat: report 생성 시 userId 추가
    - association 테이블 맵핑 (report, user)
    - ReportDto추가
      - user정보 중 userId만 사용하도록 transform decorator 사용
    - serialize decorator 활용하여  Dto로 파싱하도록 수정

--- src/server/reports/reports.controller.ts
+++ src/server/reports/reports.controller.ts
@@ -2,13 +2,18 @@ import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { CreateReportDto } from "./dtos/create-report-dto";
 import { ReportsService } from "./reports.service";
 import { AuthGuard } from "../guards/auth.guard";
+import { CurrentUser } from "../users/decorators/current-user.decorator";
+import { User } from "../users/user.entity";
+import { ReportDto } from "./dtos/report.dto";
+import { Serialize } from "../interceptors/serialize.interceptor";
 
 @Controller("reports")
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
